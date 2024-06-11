91a65e50c1246e07dbdf737a0d89b623430d110e
kensui1223 <ykgg09d2@gmail.com>
2024-06-11 21:16:38 +0900

    Delete GAKUSEI directory

--- GAKUSEI/gakuseikanri/Forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from gakuseikanri.models import Gakuseikanri
-from django.forms import fields
-class GakuseikanriForm(forms.ModelForm):
-    class Meta:
-        model = Gakuseikanri
-        fields = "__all__"
